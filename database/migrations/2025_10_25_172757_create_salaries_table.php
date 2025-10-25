7fe832d533c4bfe018f8d33f7c1ef8f7ea2dab80
Turki-Alrowithi <turkigalr@gmail.com>
2025-10-25 20:42:42 +0300

    chore: replace old repo with new Laravel Hisbah project

---
+++ database/migrations/2025_10_25_172757_create_salaries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('salary_amount', 10, 2);
+            $table->decimal('daily_hours', 5, 2);
+            $table->unsignedTinyInteger('days_per_week');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
